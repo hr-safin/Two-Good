3b1339454b6194d378f9697a2df564698da803cf
unknown <hrsafin2434@gmail.com>
2023-12-31 17:49:24 +0600

    page3 responsive done

--- script.js
+++ script.js
@@ -49,6 +49,12 @@ function loadingAnimation(){
         delay : 1.6,
         duration : .6,
     })
+    gsap.from("#page3 #child1",{
+        y : 100,
+        opacity : 0,
+        delay : 0.6,
+        duration : 0.5,
+    })
 }
 
 loadingAnimation()
